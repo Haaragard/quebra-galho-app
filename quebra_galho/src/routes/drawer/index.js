a3d65926dc1545995fae4f3af3788f33596715cb
Haaragard <diogololz159@gmail.com>
2019-10-21 19:28:17 -0300

    Commit parcial, ajustando navegação com Redux conectado

--- quebra_galho/src/routes/drawer/index.js
+++ quebra_galho/src/routes/drawer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import {drawerStyles} from '../../styles/DefaultStyles';
@@ -32,48 +32,56 @@ const btnMenuLogout = (
   </TouchableOpacity>
 );
 
-const DrawerContent = ({user}) => (
-  <View style={drawerStyles.container}>
-    <View style={drawerStyles.header}>
-      <View style={drawerStyles.containerUser}>
-        <View style={drawerStyles.containerUserImg}>
-          <Image />
+class DrawerContent extends Component {
+  render() {
+    return (
+      <View style={drawerStyles.container}>
+        <View style={drawerStyles.header}>
+          <View style={drawerStyles.containerUser}>
+            <View style={drawerStyles.containerUserImg}>
+              <Image />
+            </View>
+            <View style={drawerStyles.containerUserData}>
+              <View style={drawerStyles.containerGroupUserData}>
+                <Text style={drawerStyles.textUserDataLabel}>Nome:</Text>
+                <Text style={drawerStyles.textUserData}>{user.user.nome}</Text>
+              </View>
+              <View style={drawerStyles.containerGroupUserData}>
+                <Text style={drawerStyles.textUserDataLabel}>CPF:</Text>
+                <Text style={drawerStyles.textUserData}>{user.user.cpf}</Text>
+              </View>
+            </View>
+          </View>
         </View>
-        <View style={drawerStyles.containerUserData}>
-          <View style={drawerStyles.containerGroupUserData}>
-            <Text style={drawerStyles.textUserDataLabel}>Nome:</Text>
-            <Text style={drawerStyles.textUserData}>{user.user.nome}</Text>
-          </View>
-          <View style={drawerStyles.containerGroupUserData}>
-            <Text style={drawerStyles.textUserDataLabel}>CPF:</Text>
-            <Text style={drawerStyles.textUserData}>{user.user.cpf}</Text>
-          </View>
+        <View style={drawerStyles.content}>
+          <TouchableOpacity>
+            <View style={drawerStyles.containerMenuLine}>
+              <Text>Menu 1</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View style={drawerStyles.containerMenuLine}>
+              <Text>Menu 2</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View style={drawerStyles.containerMenuLine}>
+              <Text>Menu 3</Text>
+            </View>
+          </TouchableOpacity>
+
+          {user.user.id ? btnMenuLogout : btnMenuLogin}
         </View>
       </View>
-    </View>
-    <View style={drawerStyles.content}>
-      <TouchableOpacity>
-        <View style={drawerStyles.containerMenuLine}>
-          <Text>Menu 1</Text>
-        </View>
-      </TouchableOpacity>
+    );
+  }
+}
 
-      <TouchableOpacity>
-        <View style={drawerStyles.containerMenuLine}>
-          <Text>Menu 2</Text>
-        </View>
-      </TouchableOpacity>
+// const DrawerContent = ({user}) => (
 
-      <TouchableOpacity>
-        <View style={drawerStyles.containerMenuLine}>
-          <Text>Menu 3</Text>
-        </View>
-      </TouchableOpacity>
-
-      {user.user.id ? btnMenuLogout : btnMenuLogin}
-    </View>
-  </View>
-);
+// );
 
 const mapStateToProps = state => ({
   user: state.user,
