5961ed3c4e425e2937f2f91a594e68efb5f45244
Haaragard <diogololz159@gmail.com>
2019-10-21 21:09:27 -0300

    Ajustado props de navigation e state de redux nos components com connect - Rotas

--- quebra_galho/src/routes/drawer/index.js
+++ quebra_galho/src/routes/drawer/index.js
@@ -4,51 +4,31 @@ import {connect} from 'react-redux';
 import {drawerStyles} from '../../styles/DefaultStyles';
 import Icon from 'react-native-vector-icons/Entypo';
 
-const componentHeader = ({user}) => (
-  <View style={drawerStyles.header}>
-    <View style={drawerStyles.containerUser}>
-      <View style={drawerStyles.containerUserImg}>
-        <Image />
-      </View>
-      <View style={drawerStyles.containerUserData}>
-        <View style={drawerStyles.containerGroupUserData}>
-          <Text style={drawerStyles.textUserDataLabel}>Nome:</Text>
-          <Text style={drawerStyles.textUserData}>{user.user.nome}</Text>
-        </View>
-        <View style={drawerStyles.containerGroupUserData}>
-          <Text style={drawerStyles.textUserDataLabel}>CPF:</Text>
-          <Text style={drawerStyles.textUserData}>{user.user.cpf}</Text>
-        </View>
-      </View>
-    </View>
-  </View>
-);
-
-const componentAuth = ({user}) => {
-  return {
-    auth: user.auth,
-    button: (
-      <View style={drawerStyles.containerMenuLine}>
-        <View style={drawerStyles.containerIcon}>
-          <Icon
-            name={this.user.auth ? 'log-out' : 'login'}
-            size={30}
-            color="#000"
-          />
-        </View>
-        <Text style={drawerStyles.textMenu}>
-          {user.auth ? 'Logout' : 'Login'}
-        </Text>
-      </View>
-    ),
-  };
-};
-
-export default class DrawerContent extends Component {
+class DrawerContent extends Component {
   render() {
     return (
       <View style={drawerStyles.container}>
-        <DrawerHeader />
+        <View style={drawerStyles.header}>
+          <View style={drawerStyles.containerUser}>
+            <View style={drawerStyles.containerUserImg}>
+              <Image />
+            </View>
+            <View style={drawerStyles.containerUserData}>
+              <View style={drawerStyles.containerGroupUserData}>
+                <Text style={drawerStyles.textUserDataLabel}>Nome:</Text>
+                <Text style={drawerStyles.textUserData}>
+                  {this.props.user.user.nome}
+                </Text>
+              </View>
+              <View style={drawerStyles.containerGroupUserData}>
+                <Text style={drawerStyles.textUserDataLabel}>CPF:</Text>
+                <Text style={drawerStyles.textUserData}>
+                  {this.props.user.user.cpf}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
         <View style={drawerStyles.content}>
           <TouchableOpacity>
             <View style={drawerStyles.containerMenuLine}>
@@ -67,13 +47,25 @@ export default class DrawerContent extends Component {
               <Text>Menu 3</Text>
             </View>
           </TouchableOpacity>
+
           <TouchableOpacity
             onPress={() => {
-              DrawerAuths.auth
+              this.props.user.auth
                 ? this.props.navigation.navigate('Logout')
                 : this.props.navigation.navigate('Login');
             }}>
-            <DrawerAuths.button />
+            <View style={drawerStyles.containerMenuLine}>
+              <View style={drawerStyles.containerIcon}>
+                <Icon
+                  name={this.props.user.auth ? 'log-out' : 'login'}
+                  size={30}
+                  color="#000"
+                />
+              </View>
+              <Text style={drawerStyles.textMenu}>
+                {this.props.user.auth ? 'Logout' : 'Login'}
+              </Text>
+            </View>
           </TouchableOpacity>
         </View>
       </View>
@@ -81,10 +73,9 @@ export default class DrawerContent extends Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state, props) => ({
+  ...props,
   user: state.user,
 });
 
-// export default DrawerContent;
-const DrawerHeader = connect(mapStateToProps)(componentHeader);
-const DrawerAuths = connect(mapStateToProps)(componentAuth);
+export default connect(mapStateToProps)(DrawerContent);
