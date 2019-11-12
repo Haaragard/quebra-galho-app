21031a45081d32840a4629a3496c370c6e18dd88
Haaragard <diogololz159@gmail.com>
2019-11-12 10:00:20 -0300

    Ajuste visualização de user avatar

--- quebra_galho/src/api.js
+++ quebra_galho/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 //Online
-// const endereco = 'http://quebra-galho-backend.herokuapp.com';
-// export const BASEURL = 'http://quebra-galho-backend.herokuapp.com';
-// export const BASEURLIMG = 'http://quebra-galho-backend.herokuapp.com/images/';
-// export const BASE_URL_IMG_AVATAR_USER =
-// 'http://quebra-galho-backend.herokuapp.com/images/user/avatar';
+const endereco = 'http://quebra-galho-backend.herokuapp.com';
+export const BASEURL = 'http://quebra-galho-backend.herokuapp.com';
+export const BASEURLIMG = 'http://quebra-galho-backend.herokuapp.com/images/';
+export const BASE_URL_IMG_AVATAR_USER =
+  'http://quebra-galho-backend.herokuapp.com/images/user/avatar';
 
 //Home
 // export const endereco = 'http://192.168.0.173:3001';
@@ -15,11 +15,11 @@ import axios from 'axios';
 //   'http://192.168.0.173:3001/images/user/avatar';
 
 //MY NTBK
-const endereco = 'http://192.168.137.1:3001';
-export const BASEURL = 'http://192.168.137.1:3001';
-export const BASEURLIMG = 'http://192.168.137.1:3001/images/';
-export const BASE_URL_IMG_AVATAR_USER =
-  'http://192.168.137.1:3001/images/user/avatar';
+// const endereco = 'http://192.168.137.1:3001';
+// export const BASEURL = 'http://192.168.137.1:3001';
+// export const BASEURLIMG = 'http://192.168.137.1:3001/images/';
+// export const BASE_URL_IMG_AVATAR_USER =
+//   'http://192.168.137.1:3001/images/user/avatar';
 
 const api = axios.create({
   baseURL: endereco,
